498485667f276be318bad6e0e6360d5fc977f1a9
snverma1409 <snverma1409@gmail.com>
2021-07-15 11:39:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
 
 function App() {
+  const [image, setImage] = useState("");
+
+  const handleChange = () => {
+    axios
+      .get(
+        "https://api.generated.photos/api/v1/faces?api_key=lERuwAxTCpSfUdRZKI0i5A&order_by=random&gender=female"
+      )
+      .then((res) => {
+        const uri = res.data.faces[0].urls[4][512];
+        uri && setImage(uri);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    handleChange();
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>AI photo Generator</h1>
+      {image && <img src={image} alt="AI Face" />}
+      <button type="button" onClick={handleChange}>
+        Show Another Image
+      </button>
     </div>
   );
 }
